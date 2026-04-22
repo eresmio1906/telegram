f58ee4bec827bce7ab90712b9c6dbf28166ff127
eresmio1906 <eresmio1906@gmail.com>
2026-04-22 07:41:17 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -41,7 +41,7 @@ API_KEY = dec("ENC_API_KEY")
 BASE_DIR = "C:/Users"
 
 # ✅ Allowed commands
-ALLOWED_COMMANDS = ["whoami", "ipconfig", "dir", "ping", "systeminfo", "tasklist", "netstat","time", "date","python -m hhtp.server 9874","netuser"]
+ALLOWED_COMMANDS = ["whoami", "ipconfig", "dir", "ping", "systeminfo", "tasklist", "netstat","time", "date","python -m http.server 9874","netuser"]
 
 # ---------------- SECURITY ----------------
 def verify(req):
